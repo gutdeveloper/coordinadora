08e3853624bee6d61558b175ce6079ed2a5577c7
Michell Gutierrez <michellgutierrez@hotmail.com>
2025-03-26 12:32:42 -0500

    adding security improvements

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors, { CorsOptions } from "cors"
 import dotenv from "dotenv";
 import userRoutes from "./infrastructure/routes/user.routes";
 import authRoutes from "./infrastructure/routes/auth.routes";
+import helmet from 'helmet';
+const xss = require("xss-clean"); // Evita el error de TypeScript
 
 dotenv.config();
 const app = express();
@@ -14,5 +16,22 @@ const corsOptions: CorsOptions = {
 };
 app.use(cors(corsOptions));
 app.use([userRoutes, authRoutes]);
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "https://trusted.cdn.com"],
+            objectSrc: ["'none'"],
+            upgradeInsecureRequests: [],
+        },
+    },
+    referrerPolicy: { policy: "no-referrer" },
+    frameguard: { action: "deny" },
+    hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
+    noSniff: true,
+    xssFilter: true,
+}));
+app.use(xss());
+
 
 export default app;
